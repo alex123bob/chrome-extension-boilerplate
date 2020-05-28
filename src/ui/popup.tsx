ae939643712fc05c8a985334ab425d9b36aa2a09
alex123bob <alex123bob@126.com>
2020-05-28 18:25:50 +0800

    Move theme panel into popup

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -4,11 +4,23 @@ import { DefectFields } from '../app/constants'
 
 import "../styles/popup.scss"
 
-class App extends React.Component {
+type MyProps = {}
+
+type MyState = {
+    themeColors: Array<string>,
+    displayMsg: string
+}
+
+class App extends React.Component<MyProps, MyState> {
 
     constructor (props) {
         super(props)
-        this.state = {}
+        this.state = {
+            themeColors: ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1', '#5596e6', '#3d70b2', 
+            '#41d6c3', '#42b0e2', '#261e1e', '#b7714b', '#FF8C94', '#2F9599', '#594F4F',
+            '#A7226E', '#EC2049', '#F26B38', '#F7DB4F', '#355C7D', '#A8A7A7'],
+            displayMsg: 'Choose a different background color!'
+        }
     }
 
     handleFieldCheckChange = (evt) => {
@@ -36,28 +48,56 @@ class App extends React.Component {
         })
     }
 
+    themeBtnClick(color) {
+        let me = this
+        chrome.storage.sync.set({ color: color }, function () {
+            me.setState({
+                displayMsg: `color is ${color}`
+            })
+        })
+    }
+
+    resetColor = () => {
+        let me = this
+        chrome.storage.sync.set({ color: '' }, function () {
+            me.setState({
+                displayMsg: `color is reset`
+            })
+        })
+    }
+
     render() {
+        const {themeColors} = this.state
         return (
-            <div className="configurationCt popup-padded">
-                <div className="title">Field Configuration</div>
-                {
-                    DefectFields.map((cfg, index) => {
-                        return (
-                            <div className="field" key={`${cfg.key}-${index}`}>
-                                <label className={cfg.configurable ? '' : 'disabled'}>
-                                    {cfg['key']}:
-                                    <input
-                                        name={cfg.key}
-                                        type="checkbox"
-                                        checked={cfg.required || !!this.state[cfg.key]}
-                                        disabled={!cfg.configurable}
-                                        onChange={this.handleFieldCheckChange}
-                                    />
-                                </label>
-                            </div>
-                        )
-                    })
-                }
+            <div className="ct">
+                <div className="configuration popup-padded">
+                    <div className="title">Field Configuration</div>
+                    {
+                        DefectFields.map((cfg, index) => {
+                            return (
+                                <div className="field" key={`${cfg.key}-${index}`}>
+                                    <label className={cfg.configurable ? '' : 'disabled'}>
+                                        {cfg['key']}:
+                                        <input
+                                            name={cfg.key}
+                                            type="checkbox"
+                                            checked={cfg.required || !!this.state[cfg.key]}
+                                            disabled={!cfg.configurable}
+                                            onChange={this.handleFieldCheckChange}
+                                        />
+                                    </label>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="theme">
+                    {themeColors.map((color, index) => {
+                        return <button key={index} onClick={this.themeBtnClick.bind(this, color)} style={{backgroundColor: color}}></button>
+                    })}
+                    <p className="display">{this.state.displayMsg}</p>
+                    <button onClick={this.resetColor} className="reset">Reset Color</button>
+                </div>
             </div>
         )
     }
